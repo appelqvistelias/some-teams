ab407a2034e1ad96c693e6feb79dd546abcbe728
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-10-03 15:57:18 +0200

    added structural elements and some text

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . "/header.php"; ?>
+
+<div class="main-container">
+    <div class="text-container">
+        <h2>About me</h2>
+        <p>This will contain an informative text about me in a near future, for now this is simply a placeholder for me to play around with</p>
+    </div>
+</div>
