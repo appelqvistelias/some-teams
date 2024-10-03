c283d4c063ac424f10c5930d53c70d5eda6fbbb8
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-10-03 17:53:00 +0200

    updated the about section

--- about.php
+++ about.php
@@ -5,6 +5,8 @@ require_once __DIR__ . "/header.php"; ?>
 <div class="main-container">
     <div class="text-container">
         <h2>About me</h2>
-        <p>This will contain an informative text about me in a near future, for now this is simply a placeholder for me to play around with</p>
+        <p>Hello world! My name is Elias, and I am not very interested in football, or any sport for that matter. What I am interested in, however, is web development, which is why this site is built (apart from being an assigment that is). I would much rather play some video games or work out myself than watch sports. It is not unlikely that I'm missing out, but I do not think so. Anyway, I really hope this site is enjoyable for those who are interested in football.</p>
     </div>
 </div>
+
+<?php require_once __DIR__ . "/footer.php";
