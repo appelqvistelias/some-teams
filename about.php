667825a14b2ecf803dccaff05239cf8eeac55465
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-10-03 18:12:43 +0200

    fixed some typos

--- about.php
+++ about.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/header.php"; ?>
 <div class="main-container">
     <div class="text-container">
         <h2>About me</h2>
-        <p>Hello world! My name is Elias, and I am not very interested in football, or any sport for that matter. What I am interested in, however, is web development, which is why this site is built (apart from being an assigment that is). I would much rather play some video games or work out myself than watch sports. It is not unlikely that I'm missing out, but I do not think so. Anyway, I really hope this site is enjoyable for those who are interested in football.</p>
+        <p>Hello, world! My name is Elias, and I am not very interested in football, or any sport for that matter. What I am interested in, however, is web development, which is why this site was built (aside from it being an assignment). I would much rather play video games or work out myself than watch sports. While it’s possible that I’m missing out, I don’t think so. Anyway, I really hope this site is enjoyable for those who are interested in football.</p>
     </div>
 </div>
 
