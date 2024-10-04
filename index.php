0bf699fa19b2a09c330d2f80682e2292015625f9
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-10-04 16:57:59 +0200

    changed display flex to grid

--- index.php
+++ index.php
@@ -10,30 +10,28 @@ require_once __DIR__ . "/data.php"; ?>
         <h2>The Teams</h2>
         <p>Information about womens league football teams. Click the team names to visit official website.</p>
     </div>
-    <div class="team-info-cards-container">
-        <?php foreach ($teams as $teamName => $team) { ?>
-            <article class="team-info-card">
-                <a href="<?= $team['url'] ?>" target=_blank>
-                    <h2><?= $teamName; ?></h2>
-                </a>
-                <img src="<?= $team['logo'] ?>" height="80px" width="80px">
-                <ul>
-                    <li>
-                        <strong><?= "League: " ?></strong><?= stripslashes($team['league']); ?>
-                    </li>
-                    <li>
-                        <strong><?= "UEFA Ranking: " ?></strong><?= $team['uefa-coefficient-ranking']; ?>
-                    </li>
-                    <li>
-                        <strong><?= "City: " ?></strong><?= $team['city']; ?>
-                    </li>
-                    <li>
-                        <strong><?= "Group: " ?></strong><?= $team['group']; ?>
-                    </li>
-                </ul>
-            </article>
-        <?php } ?>
-    </div>
+    <?php foreach ($teams as $teamName => $team) { ?>
+        <article class="team-info-card">
+            <a href="<?= $team['url'] ?>" target=_blank>
+                <h2><?= $teamName; ?></h2>
+            </a>
+            <img src="<?= $team['logo'] ?>" height="80px" width="80px">
+            <ul>
+                <li>
+                    <strong><?= "League: " ?></strong><?= stripslashes($team['league']); ?>
+                </li>
+                <li>
+                    <strong><?= "UEFA Ranking: " ?></strong><?= $team['uefa-coefficient-ranking']; ?>
+                </li>
+                <li>
+                    <strong><?= "City: " ?></strong><?= $team['city']; ?>
+                </li>
+                <li>
+                    <strong><?= "Group: " ?></strong><?= $team['group']; ?>
+                </li>
+            </ul>
+        </article>
+    <?php } ?>
 
 </div>
 
