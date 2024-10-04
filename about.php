2cb43853a8ee99c0711970664d09d813fc53dc36
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-10-04 22:32:35 +0200

    redesigned the site to use grid instead of flex

--- about.php
+++ about.php
@@ -2,11 +2,9 @@
 
 require_once __DIR__ . "/header.php"; ?>
 
-<div class="main-container">
-    <div class="text-container">
-        <h2>About me</h2>
-        <p>Hello, world! My name is Elias, and I am not very interested in football, or any sport for that matter. What I am interested in, however, is web development, which is why this site was built (aside from it being an assignment). I would much rather play video games or work out myself than watch sports. While it’s possible that I’m missing out, I don’t think so. Anyway, I really hope this site is enjoyable for those who are interested in football.</p>
-    </div>
+<div class="text-info-box">
+    <h2>About me</h2>
+    <p>Hello, world! My name is Elias, and I am not very interested in football, or any sport for that matter. What I am interested in, however, is web development, which is why this site was built (aside from it being an assignment). I would much rather play video games or work out myself than watch sports. While it’s possible that I’m missing out, I don’t think so. Anyway, I really hope this site is enjoyable for those who are interested in football.</p>
 </div>
 
 <?php require_once __DIR__ . "/footer.php";
--- index.php
+++ index.php
@@ -3,17 +3,16 @@
 require_once __DIR__ . "/header.php";
 
 require_once __DIR__ . "/data.php"; ?>
+<div class="text-info-box">
+    <h2>The Teams</h2>
+    <p>Information about womens league football teams. Click the team names to visit official website.</p>
+</div>
 
-<div class="main-container">
-
-    <div class="text-container">
-        <h2>The Teams</h2>
-        <p>Information about womens league football teams. Click the team names to visit official website.</p>
-    </div>
+<div class="team-info-cards-grid">
     <?php foreach ($teams as $teamName => $team) { ?>
         <article class="team-info-card">
             <a href="<?= $team['url'] ?>" target=_blank>
-                <h2><?= $teamName; ?></h2>
+                <h2 class="team-name-h2"><?= $teamName; ?></h2>
             </a>
             <img src="<?= $team['logo'] ?>" height="80px" width="80px">
             <ul>
@@ -32,7 +31,6 @@ require_once __DIR__ . "/data.php"; ?>
             </ul>
         </article>
     <?php } ?>
-
 </div>
 
 <?php require_once __DIR__ . "/footer.php";
